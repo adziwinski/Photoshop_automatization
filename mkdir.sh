34605c39a8796dab8682cc314de6a857c01a34f6
Andrzej Dziwinski <adziwinski@gmail.com>
2019-05-29 11:46:01 +0200

    mkdir files

---
+++ mkdir.sh
@@ -0,0 +1,12 @@
+#call this file with gitbash in repo directory
+cd ../
+mkdir BMP
+cd BMP
+mkdir POLSKIE
+mkdir ANGIELSKIE
+mkdir NIEMIECKIE
+mkdir ROSYJSKIE
+mkdir UKRAINSKIE
+mkdir FRANCUSKIE
+mkdir HISZPANSKIE
+mkdir WLOSKIE
